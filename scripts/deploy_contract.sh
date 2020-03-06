9d0d6277862c94b2ab5a4421c86dda3a9c58fa0e
yangzhao <yangzhao@mcarlo.com>
2020-03-06 15:50:07 +0800

    update

--- scripts/deploy_contract.sh
+++ scripts/deploy_contract.sh
@@ -1,15 +1,11 @@
-export MAI2JS_HOME=$(pwd)
-export ADDRESS_OUTPUT_PATH=/tmp/addresses.json
-export WRAPPER_OUTPUT_PATH=${MAI2JS_HOME}/tests/eth_address.ts
+NPM=npm
 
-git clone https://b48f6bb8411720b36cb7c8eaca1cc569ba73be71@github.com/mcarloai/mai-protocol-v2.git
+git clone https://${CHECKOUT_PAT}@github.com/mcarloai/mai-protocol-v2.git
 cd mai-protocol-v2
 git checkout v2.5
 
-cnpm install
-cnpm run compile
+${NPM} install
+${NPM} run compile
 
 sed -i "s/migrations_directory.*/migrations_directory: \"test_migrations\",/g" truffle.js
-cnpm run migrate
-
-cd ${MAI2JS_HOME}
+${NPM} run migrate
