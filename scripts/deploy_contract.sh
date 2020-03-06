1096bd15a8ed5e53107b61aee5eb5d434b8d587d
yangzhao <yangzhao@mcarlo.com>
2020-03-06 16:08:59 +0800

    aupdate

--- scripts/deploy_contract.sh
+++ scripts/deploy_contract.sh
@@ -1,4 +1,8 @@
 NPM=npm
+PWD=$(pwd)
+
+echo "ADDRESS TMP:" ${ADDRESS_OUTPUT_PATH}
+echo "OUTPUT PATH:" ${WRAPPER_OUTPUT_PATH}
 
 git clone https://${CHECKOUT_PAT}@github.com/mcarloai/mai-protocol-v2.git
 cd mai-protocol-v2
